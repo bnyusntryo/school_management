9ead5b6a3c1d4e37299cdf418a5b28bf6bd1b861
reonaldi152 <reonaldi152@gmail.com>
2026-03-09 20:07:34 +0700

    change route to basepage when success login

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:school_management/view/base_page.dart';
 import '../config/pref.dart';
 import '../viewmodel/auth_viewmodel.dart';
 import 'home_page.dart';
@@ -58,7 +59,7 @@ class _LoginPageState extends State<LoginPage> {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-          builder: (_) => const HomePage(),
+          builder: (_) => const BasePage(),
         ),
       );
 
